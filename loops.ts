69d354f1c01b27d0d0735845aba9dad70fb89c67
Denisov Anton <159169213+Denisov96@users.noreply.github.com>
2024-04-24 17:27:18 +0000

    HM 24.04.24

--- loops.ts
+++ loops.ts
@@ -36,22 +36,51 @@ for (let i = 0; i < artistsArray.length; i++) {
 ///console.log(`The most listened-to artist is ${mostListenedArtist} with ${maxListeners} listeners.`);
 
 function findLargestNumber(numbers: number[]): number {
-let result = numbers[0]
-for (let i = 1; i < numbers. length; i++) {
-    const number = numbers [i]
-    if (number > result) {
-    result = number; 
- }
+    let result = numbers[0]
+    for (let i = 1; i < numbers.length; i++) {
+        const number = numbers[i]
+        if (number > result) {
+            result = number;
+        }
+    }
+    return result
 }
-return result
-}
-const ages = [22,44,77,66666,8889,9095]
+const ages = [22, 44, 77, 66666, 8889, 9095]
 const largestAge = findLargestNumber(ages)
 ///console.log(largestAge)
 
-function multiplicationNumbers(a:number, b:number, c:number){
-    return a*b*c
+function multiplicationNumbers(a: number, b: number, c: number) {
+    return a * b * c
 }
-    const reference = multiplicationNumbers
-    const resultOf =  multiplicationNumbers (5,7, 5/6)
-    console.log(`The multiplication of numbers 5,7, 5/6 is ${resultOf} `)
+const reference = multiplicationNumbers
+const resultOf = multiplicationNumbers(5, 7, 5 / 6)
+///console.log(`The multiplication of numbers 5,7, 5/6 is ${resultOf} `)
+
+function minutesConvent(minutes: number): string {
+    if (minutes === 0) {
+        return ""
+    } else {
+        const days: number = Math.floor(minutes / (24 * 60))
+        return `${days}д`
+    }
+}
+console.log(minutesConvent(459000))
+
+function minConvent(minutes: number): string {
+    if (minutes === 0) {
+        return ""
+    } else {
+        const hours: number = Math.floor(minutes / 60)
+        return `${hours}ч`
+    }
+}
+console.log(minConvent(4000))
+
+function fullConvert(minutes: number): string {
+    const days = Math.floor(minutes / (24 * 60));
+    const hours = Math.floor((minutes % (24 * 60)) / 60);
+    const remainingMinutes = minutes % 60;
+    
+    return `${days}д ${hours}ч ${remainingMinutes}м`;
+}
+console.log (fullConvert(500000))
